79d4a3c05478763eaf03507f9d2181b097075a3c
Andras Serfozo <subztep@gmail.com>
2026-02-20 13:57:19 +0000

    Simpler scraped model

--- src/schemas/scraped_job.ts
+++ src/schemas/scraped_job.ts
@@ -1,22 +1,13 @@
 import z from "zod"
 
+// FIXME: there are much more available fields
 export const ScrapedJobSchema = z.object({
   job_url: z.url(),
   site: z.string(),
   title: z.string(),
   description: z.string(),
   company: z.string(),
-  location: z.string(),
-  job_type: z.string().nullable(),
-  date_posted: z.number().nullable(),
-  interval: z.string().nullable(),
-  min_amount: z.string().nullable(),
-  max_amount: z.string().nullable(),
-  currency: z.string().nullable(),
-  is_remote: z.string().nullable(),
-  num_urgent_words: z.number().nullable(),
-  benefits: z.string().nullable(),
-  emails: z.string().nullable()
+  location: z.string()
 })
 
 export type ScrapedJob = z.infer<typeof ScrapedJobSchema>
